9e804ad619f003c006714a860ec17a349d00c1da
benshu <benshu@benshu.de>
2015-03-26 09:28:43 +0100

    Added `Promise` polyfill for IE10.

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,11 +1,14 @@
 define([
         'domReady',
+        'es6-promise',
         'knockout',
         './data/countries-by-population/data-source-factory',
         './example',
         'ko-grid-bundle'
     ],
-    function (domReady, ko, countriesByPopulation) {
+    function (domReady, es6promise, ko, countriesByPopulation) {
+        es6promise.polyfill();
+
         domReady(function () {
             ko.applyBindings({
                 countriesByPopulation: countriesByPopulation
